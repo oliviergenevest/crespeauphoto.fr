fb92238c31c6e7e1e56f6007af0250206b8afc11
oliviergenevest <olivier.genevest@gmail.com>
2019-03-09 19:31:32 +0100

    Initial commit from gatsby: (https://github.com/LekoArts/gatsby-starter-portfolio-jodie.git)

---
+++ src/elements/index.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+import { animated } from 'react-spring'
+
+import {
+  space,
+  color,
+  width,
+  flex,
+  alignSelf,
+  flexWrap,
+  flexDirection,
+  alignItems,
+  justifyContent,
+} from 'styled-system'
+
+export const Box = styled<any>('div')`
+  box-sizing: border-box;
+  ${space}
+  ${width}
+  ${color}
+  ${flex}
+  ${alignSelf}
+`
+
+Box.displayName = 'Box'
+
+export const AnimatedBox = styled<any>(animated.div)`
+  box-sizing: border-box;
+  ${space}
+  ${width}
+  ${color}
+  ${flex}
+  ${alignSelf}
+`
+
+AnimatedBox.displayName = 'AnimatedBox'
+
+export const Flex = styled<any>('div')`
+  display: flex;
+  ${space}
+  ${flexWrap}
+  ${flexDirection}
+  ${alignItems}
+  ${justifyContent}
+`
+
+Flex.displayName = 'Flex'
+
+export const AnimatedFlex = styled<any>(animated.div)`
+  display: flex;
+  ${space}
+  ${flexWrap}
+  ${flexDirection}
+  ${alignItems}
+  ${justifyContent}
+`
+
+AnimatedFlex.displayName = 'AnimatedFlex'
+
+export const Button = styled<any>('button')`
+  border-radius: 1000rem;
+  border: none;
+  font-weight: 700;
+  font-size: 1.25rem;
+  ${space}
+  ${width}
+  ${color}
+`
+
+Button.displayName = 'Button'
